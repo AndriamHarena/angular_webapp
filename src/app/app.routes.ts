ed0956fe4bfd349ca74aad09261e700101cf40c5
AndriamHarena <andriamananjaramandimbyharena@gmail.com>
2025-08-19 13:59:52 +0200

    fix: Update default route to redirect to home instead of empty path

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -6,10 +6,10 @@ import { AboutComponent } from './pages/about/about.component';
 import { ContactComponent } from './pages/contact/contact.component';
 
 export const routes: Routes = [
-  { path: '', component: HomeComponent },
+  { path: 'home', component: HomeComponent },
   { path: 'about', component: AboutComponent },
   { path: 'contact', component: ContactComponent },
   { path: 'login', component: LoginComponent },
   { path: 'register', component: RegisterComponent },
-  { path: '**', redirectTo: '' }
+  { path: '**', redirectTo: 'home' }
 ];
